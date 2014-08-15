4df0c670eba9a74f5046c672201fb84462fc8bc1
dexter <dexter.genterone@gmail.com>
2014-08-15 17:06:51 +0800

    influxdb only with graphite support and config
    - removed graphite-api
    - updated bootstrap to use database.json
    - shard spaces definitions for graphite db
    - added continuous queries for graphite db

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
 FROM ubuntu:trusty
 MAINTAINER Acaleph <admin@acale.ph>
 
-# the universe is broken. thank you https://github.com/docker/docker/issues/5388 for the fix.
-#RUN echo "deb http://archive.ubuntu.com/ubuntu trusty main universe" > /etc/apt/sources.list
-
 # Install InfluxDB
 RUN apt-get update
 RUN apt-get upgrade -y
 
-RUN apt-get install -y language-pack-en wget
+RUN apt-get install -y language-pack-en wget curl
 ENV LANGUAGE en_US.UTF-8
 ENV LANG en_US.UTF-8
 ENV LC_ALL en_US.UTF-8
@@ -18,44 +15,15 @@ RUN dpkg-reconfigure locales
 
 RUN wget http://s3.amazonaws.com/influxdb/influxdb_0.8.0-rc.4_amd64.deb && dpkg -i influxdb_0.8.0-rc.4_amd64.deb
 
-RUN apt-get install -y build-essential python-dev libffi-dev libcairo2-dev python-pip supervisor curl
+ADD ./influxdb.conf /usr/local/etc/influxdb.conf
+ADD ./database.json /usr/local/etc/database.json
+ADD ./continuous_queries.dat /usr/local/etc/continuous_queries.dat
 
-RUN pip install gunicorn Flask-Cache statsd raven blinker
-
-# patched version with cache
-RUN pip install https://github.com/Dieterbe/graphite-api/tarball/check-series-early
-
-# graphite-influxdb PR#16 which fixes metrics api
-RUN pip install https://github.com/svanharmelen/graphite-influxdb/tarball/bugfix
-
-# latest influxdb-python
-RUN pip uninstall -y influxdb
-RUN pip install https://github.com/influxdb/influxdb-python/tarball/master
-
-# add graphite-api config
-ADD graphite-api.yaml /etc/graphite-api.yaml
-RUN chmod 0644 /etc/graphite-api.yaml
-
-# bootstrap to add graphite db
 ADD ./bootstrap.sh /bootstrap.sh
 RUN chmod 0744 /bootstrap.sh
 
-ADD ./influxdb.conf /usr/local/etc/influxdb.conf
-ADD ./supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-# HACK. added 1 second interval.
-ADD ./maintain_cache.py /usr/local/bin/maintain_cache.py
-RUN chmod 0744 /usr/local/bin/maintain_cache.py
-
-RUN mkdir /srv/graphite && chmod 777 /srv/graphite
-
 # cleanup
 RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-
-# Graphite
-EXPOSE 2003
-EXPOSE 2003/udp
 
 # Admin
 EXPOSE 8083
@@ -69,12 +37,10 @@ EXPOSE 8090
 # Replication
 EXPOSE 8099
 
-# graphite-api
-EXPOSE 8000
+# Graphite
+EXPOSE 2003
+EXPOSE 2003/udp
 
-# VOLUME [ "/opt/influxdb/shared/data/db" ]
-# VOLUME [ "/opt/graphite" ]
-VOLUME [ "/var/log/supervisor", "/var/lib/influxdb" ]
+VOLUME /var/lib/influxdb
 
-
-CMD ["/usr/bin/supervisord"]
+CMD ["/bootstrap.sh"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,94 @@
-#!/bin/sh
+#!/bin/bash -e
+ 
+# the influxdb binary
+influxdb_bin=/usr/bin/influxdb
 
-# This is just a quick script to create the graphite database and graphite user.
-# This is executed under supervisor once.
+# influxdb host and port
+influxdb_host=localhost
+influxdb_api_port=8086
 
-# Sleep for a while to make sure that influxdb has totally started up
-echo 'waiting for influxdb to bootup. (cheat: just 10 second wait)'
-sleep 10
+# the root password. if not specified, will default to root
+influxdb_root_password=${INFLUXDB_ROOT_PASSWORD:=root}
+
+# location of config and data files
+influxdb_config=/usr/local/etc/influxdb.conf
+influxdb_db_config=/usr/local/etc/database.json
+continuous_queries_data=/usr/local/etc/continuous_queries.dat
 
 
-echo 'creating graphite database'
-# create graphite database
-curl -X POST -d '{"name": "graphite"}' 'http://localhost:8086/db?u=root&p=root'
+# start influxdb, if a parameter is given, influxdb will be 
+# ran in the background
+function start_influxdb() {
+	if [[ -n "$1" ]]; then
+		$influxdb_bin --config=$influxdb_config &
+		sleep 10
+	else
+		$influxdb_bin --config=$influxdb_config
+	fi
+}
 
-echo 'creating graphite user'
-# create graphite user
-curl -X POST -d '{"name": "graphite", "password": "graphite"}' 'http://localhost:8086/db/graphite/users?u=root&p=root'
 
-# sigh.. need data for maintain_cache to work or else it fails miserably.
-echo "bootstrap 1 `date +%s`" | nc -q0 localhost 2003
 
-# maintain cache
-echo 'running cache maintenance'
-exec /usr/local/bin/maintain_cache.py > /dev/null
+# check if a database already exists. returns 0 if it exists. 
+# else it returns 1
+function db_exist() {
+	echo "checking if database $1 exists..."
+	db_list=`curl -s "http://${influxdb_host}:${influxdb_api_port}/db?u=root&p=${influxdb_root_password}"`
+	if [[ $db_list == *$1* ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+
+# change the root password to the value of ${influxdb_root_password}
+function change_root_password() {
+	already_changed=`curl -s -o /dev/null -w %{http_code} "http://${influxdb_host}:${influxdb_api_port}/db?u=root&p=${influxdb_root_password}"`
+	if [[ "$already_changed" == "200" ]]; then
+		echo 'password already changed'
+	else
+		change_success=`curl -s -o /dev/null -w %{http_code} "http://${influxdb_host}:${influxdb_api_port}/cluster_admins/root?u=root&p=root" -d "{\"password\": \"${influxdb_root_password}\"}"`
+		if [[ "$change_success" == "200" ]]; then
+			echo 'root password changed'
+		else
+			echo $change_success
+			echo 'unable to change password.'
+			exit 1
+		fi
+	fi
+}
+
+
+
+# setup the graphite database.
+function setup_graphite() {
+	if ! db_exist graphite; then
+		echo 'loading configuration'
+		$influxdb_bin -load-database-config $influxdb_db_config -load-server="${influxdb_host}:${influxdb_api_port}" -load-user="root" -load-password="${influxdb_root_password}"
+		echo 'creating graphite user'
+		curl -s -o /dev/null -X POST -d '{"name": "graphite", "password": "graphite"}' "http://${influxdb_host}:${influxdb_api_port}/db/graphite/users?u=root&p=${influxdb_root_password}"
+		echo 'create graphite continuous_queries'
+		while read i; do
+			echo "creating continuous query: ${i}"
+			curl -s -o /dev/null -G "http://${influxdb_host}:${influxdb_api_port}/db/graphite/series?u=root&p=${influxdb_root_password}" --data-urlencode "q=${i}"
+		done < ${continuous_queries_data}
+	else
+		echo 'graphite db already exists.'
+	fi
+}
+
+
+# main function
+function main() {
+	start_influxdb background
+	change_root_password
+	setup_graphite
+	kill $! 	# kill influxdb
+	start_influxdb
+}
+
+
+# run main
+main
